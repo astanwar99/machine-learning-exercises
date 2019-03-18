6c04ea0ba4fe3731bd0b0829340a1dc6e2fcc9be
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2019-03-18 20:31:12 +0530

    Completed cost function.

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+hx = X*theta;           %theta'*X = X*theta
+sqrerr = (hx - y).^2;   %Square Error
+J = 1/(2*m)*sum(sqrerr);
 
 % =========================================================================
 
