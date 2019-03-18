ed81c343a613ae3553c1b7875653e99fed386f92
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2019-03-18 20:41:34 +0530

    Completed gradientDescentMulti function.

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,17 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    hx = X * theta;
+    err = hx - y;         %Error
+    del = X'*err;
+    theta = theta - (alpha / m) * del;
     % ============================================================
 
     % Save the cost J in every iteration    
